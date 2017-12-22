1c5c9371f3698dd226055d5af77f2ed5b01d1df8
Alex Benzer <abenzer@gmail.com>
2017-12-22 00:57:59 -0800

    Custom skybox

--- Assets/Scripts/spawnDonuts.js
+++ Assets/Scripts/spawnDonuts.js
@@ -1,26 +1,44 @@
-﻿var Donut_small : Transform;
+﻿var canCallFunction : boolean = true;
+var donutCount : int = 0;
+var Donut_small : Transform;
 var Donut_large : Transform;
 var Donut_creme : Transform;
-var x_offset : int = 0;
-var y_offset : int = 50;
 
-function Start () {
-    for (var y = 0; y < 10; y++) {
-        for (var x = 0; x < 10; x++) {
-        	var x_pos : float = x + Random.Range(-10.0f, 10.0f) + 5 + x_offset;
-        	var y_pos : float = y + Random.Range(-10.0f, 10.0f) + 5;
-        	var z_pos : float = Random.Range(-10.0f, 10.0f) + y_offset;
+//config
+var configSpeed : float = .05;  
 
-        	var donutIndex : int = Random.Range(0, 3);
-        	Debug.Log(donutIndex);
-        	if (donutIndex == 0) {
-            	Instantiate(Donut_small, Vector3 (x_pos, y_pos, z_pos), Quaternion.identity);
-        	} else if(donutIndex == 1) {
-            	Instantiate(Donut_large, Vector3 (x_pos, y_pos, z_pos + 20), Quaternion.identity);
-            } else if(donutIndex == 2) { 
-            	Instantiate(Donut_creme, Vector3 (x_pos, y_pos, z_pos + 20), Quaternion.identity);
-            }
+function Start() {
+}
 
-        }
-    }
+function Update() {
+	if(canCallFunction) {
+   		AddDonut();
+		Wait(configSpeed);
+	}
+	donutCount++;
+} 
+
+function AddDonut() {
+	x = 0;
+	y = 30;
+	var x_pos : float = x + Random.Range(-2.0f, 2.0f);
+	var y_pos : float = y + Random.Range(-2.0f, 2.0f);
+	var z_pos : float = Random.Range(-2.0f, 2.0f);
+
+	var donutIndex : int = Random.Range(0, 3);
+    if (donutIndex == 0) {
+    	Instantiate(Donut_small, Vector3 (x_pos, y_pos, z_pos), Quaternion.identity);
+    } else if(donutIndex == 1) {
+		Instantiate(Donut_large, Vector3 (x_pos, y_pos, z_pos), Quaternion.identity);
+	} else if(donutIndex == 2) { 
+    	//Instantiate(Donut_creme, Vector3 (x_pos, y_pos, z_pos), Quaternion.identity);
+	}
+}
+
+
+// delay
+function Wait(time : float) {
+	canCallFunction= false;
+	yield new WaitForSeconds(time);
+	canCallFunction= true;
 }
